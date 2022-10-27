fe17480aa9e66a29b4b9071d29133b058fbb2b19
Brian Walsh <brian@bwalsh.com>
2022-10-27 20:58:48 +0000

    Use docker-space-compose

--- guppy_setup.sh
+++ guppy_setup.sh
@@ -2,9 +2,9 @@
 # Script to create and re-create es indices and setup guppy
 
 docker exec esproxy-service curl -X DELETE http://localhost:9200/gen3.aced.*
-docker-compose stop guppy-service
-docker-compose rm -f guppy-service
+docker compose stop guppy-service
+docker compose rm -f guppy-service
 
 ./etl/tube_lite --credentials_path $1  --elastic http://localhost:9200  --batch_size 20   #  --limit 1000
 
-docker-compose up -d guppy-service
+docker compose up -d guppy-service
